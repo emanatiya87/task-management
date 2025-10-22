c3311c7b6039e0ff62653e3e3ae72b09a7cf4341
Eman <emanatiya87@gmail.com>
2025-10-22 17:48:40 +0300

    instialize next project ,TS , tailwing

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="bg-primary">
+        <h2 className="text-teal-200 text-center">hello</h2>
+      </div>
+    </>
+  );
+}
