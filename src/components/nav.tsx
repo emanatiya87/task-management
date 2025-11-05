ee2b422376aa4c2a598f052d2451513360d54c7a
Eman Atia <emanatiya87@gmail.com>
2025-11-05 03:13:52 +0200

    nav UI

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,3 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
 export default function Nav() {
-  return <div>nav</div>;
+  return (
+    <>
+      <nav className="bg-white border-gray-900 dark:bg-gray-900 py-2 px-3 shadow-md">
+        <div className="max-w-7xl flex flex-wrap items-center justify-between mx-auto ">
+          <Link
+            href="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse "
+          >
+            <Image
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+              width={30}
+              height={30}
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              AProjectO
+            </span>
+          </Link>
+
+          <div className="flex gap-2 items-center ">
+            <div className="flex flex-col items-end">
+              <h5 className="text-lg font-medium text-gray-900 dark:text-white">
+                Bonnie Green
+              </h5>
+              <h6 className="text-sm text-gray-500 dark:text-gray-400 ">
+                Visual Designer
+              </h6>
+            </div>
+            <Image
+              src="/signupimg.svg"
+              alt="Profile"
+              className="rounded-full shadow-lg"
+              width={40}
+              height={40}
+            />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
