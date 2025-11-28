446f5b5e99bc60d2fcda9b27f6e5e171fb66c2a2
Eman Atia <emanatiya87@gmail.com>
2025-11-28 21:03:39 +0200

    remove test case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import ExampleComponent from "@/components/exapmle";
 export default function Home() {
   const router = useRouter();
   const [Msg, setMsg] = useState("");
@@ -23,7 +22,6 @@ export default function Home() {
       <div className="bg-primary">
         <Link href="/registration/SignUp"> signup</Link>
         <h3 className="text-red-600">{Msg}</h3>
-        <ExampleComponent />
       </div>
     </>
   );
