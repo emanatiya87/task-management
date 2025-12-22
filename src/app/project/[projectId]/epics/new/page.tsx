e20938e19d3572e0ebbdd0e081e131eb433fd504
Eman Atia <emanatiya87@gmail.com>
2025-12-22 14:05:22 +0200

    add new epic

--- src/app/project/[projectId]/epics/new/page.tsx
+++ src/app/project/[projectId]/epics/new/page.tsx
@@ -1,3 +1,51 @@
-export default function newEpic() {
-  return <>hi</>;
+import { cookies } from "next/headers";
+import Link from "next/link";
+import { BaseUrl, ApiKey } from "@/constants/apiConstants";
+import FormAddEpic from "@/components/forms/formAddEpic";
+export default async function AddEpic({
+  params,
+}: {
+  params: Promise<{ projectId: string }>;
+}) {
+  const resolvedParams = await params;
+
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("access_token")?.value;
+
+  if (!accessToken) {
+    return (
+      <p>
+        Not authenticated <Link href="/registration/login">Login</Link>
+      </p>
+    );
+  }
+
+  try {
+    const res = await fetch(
+      `${BaseUrl}/rest/v1/projects?id=eq.${resolvedParams.projectId}`,
+      {
+        headers: {
+          apikey: ApiKey,
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const project = await res.json();
+    console.log("project", project);
+
+    return (
+      <>
+        <h2 className="mb-3 text-gray-600 text-xl font-semibold">
+          <Link href={"/project"}>Project</Link>/ {project[0].name}/{" "}
+          <Link href={`/project/${resolvedParams.projectId}/epics`}>Epics</Link>
+          /Create New
+        </h2>
+        <FormAddEpic project={project[0]} />
+      </>
+    );
+  } catch (error) {
+    console.log("Failed to fetch project", error);
+    return <p>Failed to fetch project</p>;
+  }
 }
---
+++ src/components/forms/formAddEpic.tsx
@@ -0,0 +1,149 @@
+"use client";
+import { useState } from "react";
+import { Label, TextInput } from "flowbite-react";
+import { Button } from "flowbite-react";
+import axios from "axios";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ApiKey, BaseUrl } from "@/constants/apiConstants";
+import { getAccessToken } from "@/constants/token";
+import ToastComponent from "../toast";
+import Link from "next/link";
+interface ProjectType {
+  id: string;
+  name: string;
+  description: string;
+  created_at: string;
+  deadline: string;
+}
+export default function FormAddEpic({ project }: { project: ProjectType }) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const schema = z.object({
+    name: z.string().min(3, "Title is required at least 3 chars"),
+    description: z.string().optional(),
+    deadline: z
+      .string()
+      .refine((value) => {
+        if (!value) return true;
+        const selectedDate = new Date(value);
+        selectedDate.setHours(0, 0, 0, 0);
+        return selectedDate >= today;
+      }, "Deadline must be today or in the future")
+      .optional(),
+  });
+  type projectInput = z.infer<typeof schema>;
+  const [errorMsg, setErrorMsg] = useState("");
+  const [addedSuccessfully, setAddedSuccessfully] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<projectInput>({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit: SubmitHandler<projectInput> = async (data) => {
+    console.log(data);
+    setErrorMsg("");
+    // Send a POST request
+    const accessToken = await getAccessToken();
+    axios
+      .post(
+        `${BaseUrl}/rest/v1/epics`,
+        {
+          title: data.name,
+          description: data.description,
+          assignee_id: "06dfc57d-4321-4776-8e41-964236f45302",
+          project_id: project.id,
+          deadline: data.deadline,
+        },
+        {
+          headers: {
+            apikey: ApiKey,
+            Authorization: `Bearer ${accessToken?.value}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        setAddedSuccessfully(true);
+        // Auto hide after 3 seconds
+        setTimeout(() => {
+          setAddedSuccessfully(false);
+        }, 3000);
+      })
+      .catch((error) => {
+        setErrorMsg("Failed to add epic" + error);
+      });
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="bg-white dark:bg-gray-800 rounded-2xl p-3 w-full flex flex-col gap-4 ">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="title">Title</Label>
+            </div>
+            <TextInput
+              id="title"
+              type="text"
+              sizing="sm"
+              {...register("name")}
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+            )}
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="description">Description</Label>
+            </div>
+            <TextInput
+              id="description"
+              type="text"
+              sizing="lg"
+              {...register("description")}
+            />
+            {errors.description && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.description.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="deadline">Deadline</Label>
+            </div>
+            <div className="relative max-w-sm">
+              <input
+                type="date"
+                id="deadline"
+                {...register("deadline")}
+                className="block w-full ps-3 pe-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base rounded-lg"
+              />
+            </div>
+            {errors.deadline && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.deadline.message}
+              </p>
+            )}
+          </div>
+          <div className="ms-auto flex gap-3">
+            <Button className="px-11 cursor-pointer" type="submit">
+              Create
+            </Button>
+            <Button className="px-11 cursor-pointer " color="light">
+              <Link href={"/project"}>Cancel</Link>
+            </Button>
+          </div>
+          <p className="text-red-500 text-sm mt-1 text-center">{errorMsg}</p>
+        </div>
+      </form>
+      <ToastComponent
+        message="Epic added successfully"
+        appear={addedSuccessfully}
+      />
+    </>
+  );
+}
