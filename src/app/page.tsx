7466e8a445cbc1b350b03d0cc0badfb9de291dcd
Eman Atia <emanatiya87@gmail.com>
2026-01-13 17:09:21 +0200

    remove un nesseary code for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "./store";
-import { setIsLogin } from "@/features/isLogin/isLogin";
 export default function Home() {
   const isLoginValue = useSelector((state: RootState) => state.isLogin.value);
   const dispatch = useDispatch();
@@ -28,12 +27,6 @@ export default function Home() {
       <Link href="/project/add"> add project</Link>
       <h3 className="text-red-600">{Msg}</h3>
       <h2>logged in?: {isLoginValue ? "yes" : "no"}</h2>
-      <button
-        aria-label="Increment value"
-        onClick={() => dispatch(setIsLogin(false))}
-      >
-        isLogin?
-      </button>
     </div>
   );
 }
