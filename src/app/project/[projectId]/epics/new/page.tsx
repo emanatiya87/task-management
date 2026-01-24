58cbb670b4198dab4f0f83bbf902dbe281baaf5f
Eman Atia <emanatiya87@gmail.com>
2026-01-24 23:13:48 +0200

    refactor(api): simplify requests using shared axios client in new epic page

--- src/app/project/[projectId]/epics/new/page.tsx
+++ src/app/project/[projectId]/epics/new/page.tsx
@@ -1,51 +1,55 @@
-import { cookies } from "next/headers";
+"use client";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import apiClient from "@/lib/apiClient";
+import Loading from "@/app/loading";
 import Link from "next/link";
-import { BaseUrl, ApiKey } from "@/constants/apiConstants";
 import FormAddEpic from "@/components/forms/formAddEpic";
-export default async function AddEpic({
+interface ProjectType {
+  id: string;
+  name: string;
+  description: string;
+  created_at: string;
+  deadline: string;
+}
+export default function AddEpic({
   params,
 }: {
   params: Promise<{ projectId: string }>;
 }) {
-  const resolvedParams = await params;
-
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("access_token")?.value;
-
-  if (!accessToken) {
-    return (
-      <p>
-        Not authenticated <Link href="/registration/login">Login</Link>
-      </p>
-    );
-  }
-
-  try {
-    const res = await fetch(
-      `${BaseUrl}/rest/v1/projects?id=eq.${resolvedParams.projectId}`,
-      {
-        headers: {
-          apikey: ApiKey,
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
+  const { projectId } = useParams<{ projectId: string }>();
+  const [project, setProjects] = useState<ProjectType[]>();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function getProjects() {
+      try {
+        const res = await apiClient.get(`/rest/v1/projects?id=eq.${projectId}`);
+        setProjects(res.data);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
       }
-    );
-    const project = await res.json();
-    console.log("project", project);
-
-    return (
-      <>
-        <h2 className="mb-3 text-gray-600 text-xl font-semibold">
-          <Link href={"/project"}>Project</Link>/ {project[0].name}/{" "}
-          <Link href={`/project/${resolvedParams.projectId}/epics`}>Epics</Link>
-          /Create New
-        </h2>
-        <FormAddEpic project={project[0]} />
-      </>
-    );
-  } catch (error) {
-    console.log("Failed to fetch project", error);
-    return <p>Failed to fetch project</p>;
-  }
+    }
+    getProjects();
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : project ? (
+        <>
+          <h2 className="mb-3 text-gray-600 text-xl font-semibold">
+            <Link href={"/project"}>Project</Link>/ {project[0].name}/{" "}
+            <Link href={`/project/${projectId}/epics`}>Epics</Link>
+            /Create New
+          </h2>
+          <FormAddEpic project={project[0]} />
+        </>
+      ) : (
+        <>no</>
+      )}
+    </>
+  );
 }
