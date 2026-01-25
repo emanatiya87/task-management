5b2419d1f71d5ad34ebb078913e19b6509c51ca0
Eman Atia <emanatiya87@gmail.com>
2026-01-25 20:31:25 +0200

    Display Epic Details Popup

--- src/app/project/[projectId]/epics/page.tsx
+++ src/app/project/[projectId]/epics/page.tsx
@@ -9,6 +9,7 @@ import { formatDate } from "@/utils/dateFormatter";
 import Loading from "@/app/loading";
 import { Button } from "flowbite-react";
 import apiClient from "@/lib/apiClient";
+import EpicPopup from "@/components/epicDetailsPopup";
 interface ProjectType {
   id: string;
   epic_id: string;
@@ -30,6 +31,8 @@ export default function Epics({
 }) {
   const [epics, setEpics] = useState<ProjectType[]>([]);
   const [loading, setLoading] = useState(true);
+  const [epicId, setEpicId] = useState("");
+  const [openModal, setOpenModal] = useState(false);
   const { projectId } = use(params);
   useEffect(() => {
     async function getEpics() {
@@ -38,6 +41,7 @@ export default function Epics({
           `/rest/v1/project_epics?project_id=eq.${projectId}`,
         );
         setEpics(res.data);
+        console.log(res.data);
       } catch (error) {
         console.log(error);
       } finally {
@@ -57,41 +61,54 @@ export default function Epics({
           </h2>
           <div className="bg-white dark:bg-gray-800 rounded-2xl p-3 w-full flex flex-col gap-4 overflow-x-auto md:overflow-visible">
             {epics.length > 0 ? (
-              epics.map((epic) => (
-                <div
-                  key={epic.id}
-                  className=" min-w-[700px] md:min-w-0 shadow-2xl p-3 rounded-2xl flex justify-between items-center"
-                >
-                  <div className="flex textStyle items-center gap-1">
-                    <FaRegLightbulb />
-                    <div className="flex flex-col ">
-                      <strong>{epic.title}</strong>
-                      <p className="text-gray-500">
-                        {epic.epic_id} Opened By <b>{epic.created_by?.name}</b>
+              <>
+                {epics.map((epic) => (
+                  <div
+                    key={epic.id}
+                    className="cursor-pointer min-w-[700px] md:min-w-0 shadow-2xl p-3 rounded-2xl flex justify-between items-center"
+                    onClick={() => {
+                      setOpenModal(true);
+                      setEpicId(epic.id);
+                    }}
+                  >
+                    <div className="flex textStyle items-center gap-1">
+                      <FaRegLightbulb />
+                      <div className="flex flex-col ">
+                        <strong>{epic.title}</strong>
+                        <p className="text-gray-500">
+                          {epic.epic_id} Opened By{" "}
+                          <b>{epic.created_by?.name}</b>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="text-gray-600 font-semibold dark:text-gray-300">
+                      <p>Created At</p>
+                      <div className="flex justify-between items-center gap-1">
+                        <SlCalender /> <p>{formatDate(epic.created_at)}</p>
+                      </div>
+                    </div>
+                    <div className="flex gap-0.5">
+                      <Image
+                        src="/user.png"
+                        alt="userimg"
+                        className="rounded-[50%] shadow-lg"
+                        width={30}
+                        height={20}
+                      />
+                      <p className="textStyle font-semibold">
+                        {epic?.assignee?.name}
                       </p>
                     </div>
+                    <FaEllipsisVertical className="cursor-pointer textStyle" />
                   </div>
-                  <div className="text-gray-600 font-semibold dark:text-gray-300">
-                    <p>Created At</p>
-                    <div className="flex justify-between items-center gap-1">
-                      <SlCalender /> <p>{formatDate(epic.created_at)}</p>
-                    </div>
-                  </div>
-                  <div className="flex gap-0.5">
-                    <Image
-                      src="/user.png"
-                      alt="userimg"
-                      className="rounded-[50%] shadow-lg"
-                      width={30}
-                      height={20}
-                    />
-                    <p className="textStyle font-semibold">
-                      {epic?.assignee?.name}
-                    </p>
-                  </div>
-                  <FaEllipsisVertical className="cursor-pointer textStyle" />
-                </div>
-              ))
+                ))}
+                <EpicPopup
+                  epicId={epicId}
+                  open={openModal}
+                  setOpenModal={setOpenModal}
+                  projectId={projectId}
+                />
+              </>
             ) : (
               <div className="bg-white dark:bg-gray-800 rounded-2xl p-3 w-full flex flex-col items-center justify-center gap-4 h-1/2">
                 <h2 className="textStyle text-2xl font-semibold">
---
+++ src/components/epicDetailsPopup.tsx
@@ -0,0 +1,123 @@
+"use client";
+import { Button, Modal, ModalBody, ModalHeader } from "flowbite-react";
+import { FaRegUser } from "react-icons/fa";
+import { FaRegUserCircle } from "react-icons/fa";
+import { SlCalender } from "react-icons/sl";
+import { formatDate } from "@/utils/dateFormatter";
+import { useEffect, useState } from "react";
+import apiClient from "@/lib/apiClient";
+import Loading from "@/app/loading";
+interface ProjectType {
+  id: string;
+  epic_id: string;
+  title: string;
+  description: string;
+  created_at: string;
+  assignee?: User;
+  created_by?: User;
+}
+interface User {
+  sub: string;
+  name: string;
+}
+export default function EpicPopup({
+  epicId,
+  open,
+  setOpenModal,
+  projectId,
+}: {
+  epicId: string;
+  open: boolean;
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+  projectId: string;
+}) {
+  const tasks = null;
+  const [loading, setLoading] = useState(true);
+  const [epic, setEpic] = useState<ProjectType | null>(null);
+  useEffect(() => {
+    async function getEpics() {
+      try {
+        const res = await apiClient.get(
+          `/rest/v1/project_epics?project_id=eq.${projectId}&id=eq.${epicId}`,
+        );
+        setEpic(res.data[0]);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getEpics();
+  }, [open]);
+  return (
+    <>
+      <Modal show={open} onClose={() => setOpenModal(false)}>
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <ModalHeader className="border-b-0 ">
+              <p className="text-gray-500 text-[14px]">
+                Project/{epic?.epic_id}
+              </p>
+            </ModalHeader>
+            <ModalBody className="mt-0  pt-0">
+              <h2 className="textStyle text-xl font-bold capitalize mb-3">
+                {epic?.title}
+              </h2>
+
+              <div className=" inline-grid grid-cols-[auto_auto] gap-x-6 gap-y-2  ">
+                <p className="flex items-center text-gray-700 gap-2">
+                  <FaRegUser className="text-gray-700" />
+                  <span>Created By </span>
+                </p>
+                <p>
+                  <span>M</span> <span>{epic?.created_by?.name}</span>
+                </p>
+                <p className="flex items-center text-gray-700 gap-2">
+                  <FaRegUserCircle className="text-gray-700" />
+                  <span>Assignee </span>
+                </p>
+                {epic?.assignee?.name ? (
+                  <p>
+                    <span>M</span> <span>{epic?.assignee?.name}</span>
+                  </p>
+                ) : (
+                  <>Unassigned</>
+                )}
+                <p className="flex items-center text-gray-700 gap-2">
+                  <SlCalender className="text-gray-700" />
+                  <span>Created At </span>
+                </p>
+                <p>
+                  {epic ? formatDate(epic.created_at) : "not detrmined yet"}
+                </p>
+              </div>
+              <hr className="text-gray-400 my-3.5" />
+              <h3 className=" mb-5 font-semibold textStyle">Description</h3>
+              {epic?.description ? (
+                <p className="textStyle">{epic?.description}</p>
+              ) : (
+                <p className="font-semibold"> No description provided</p>
+              )}
+              <hr className="text-gray-400 my-3.5" />
+              <h3 className=" mb-5 font-semibold textStyle">Tasks</h3>
+              <div>
+                {tasks ? (
+                  <p className="textStyle">Tasks</p>
+                ) : (
+                  <div className=" py-10 flex flex-col gap-2 items-center justify-center">
+                    <p className=" textStyle font-bold ">
+                      No tasks have been added to this epic yest
+                    </p>
+                    <Button>Create New Tasks</Button>
+                  </div>
+                )}
+              </div>
+            </ModalBody>
+          </>
+        )}
+      </Modal>
+    </>
+  );
+}
