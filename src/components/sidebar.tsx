45759a7fd0fb9ea53e7efc94d060e8b99e4ab3b8
Eman Atia <emanatiya87@gmail.com>
2026-01-27 12:24:26 +0200

    navigate to tasks of project when click on tasks

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -59,7 +59,7 @@ export default function Sidebar({ open, setOpen }: SidebarProps) {
               </li>
               <li>
                 <Link
-                  href="/"
+                  href={`/project/${projectId}/tasks`}
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <IoMdPhotos className="iconsStyle" />
