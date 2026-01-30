ce31db71494cc5431d97217a746c737084b43117
Eman Atia <emanatiya87@gmail.com>
2026-01-30 21:27:53 +0200

    remove unnessesry file

--- src/functions/updateEpic.ts
+++
@@ -1,8 +0,0 @@
-import apiClient from "@/lib/apiClient";
-export async function updateEpic(epicId: string, data: EpicUpdateForm) {
-  return apiClient.patch(`/rest/v1/epics?id=eq.${epicId}`, data, {
-    headers: {
-      Prefer: "return=representation",
-    },
-  });
-}
