be85e406c2f9a4e0a5eb3fa985d16e7d8ae4ddf4
Eman Atia <emanatiya87@gmail.com>
2026-02-02 23:36:41 +0200

    style of not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="flex flex-col items-center content-center gap-2">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
